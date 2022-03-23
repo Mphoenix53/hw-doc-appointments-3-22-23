c64d2d81c98fd71d0c1dd92bc9b4e5acf7d859da
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-23 17:07:44 -0600

    models for doctor patient appointments made and seed file setup with doctors patients and appointment data tested and working app functioning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+d1 = Doctor.create(name:"Dr Garfield")
+d2 = Doctor.create(name:"Dr Oide")
+d3 = Doctor.create(name:"Dr Hyde")
+
+p1 = Patient.create(name:"Michael")
+p2 = Patient.create(name:"Syndee")
+p3 = Patient.create(name:"Jennifer")
+
+Appointment.create(date:'May 1', doctor_id:d2.id, patient_id:p1.id)
+Appointment.create(date:'May 3', doctor_id:d3.id, patient_id:p2.id)
+Appointment.create(date:'May 21', doctor_id:d1.id, patient_id:p3.id)
