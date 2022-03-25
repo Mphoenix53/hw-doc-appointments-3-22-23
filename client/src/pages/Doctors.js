c6f4319080628899457f7a223b9e84d8dfc2dc21
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-25 15:09:13 -0600

    doctors.js rendering data when nav bar doctors is clicked

--- client/src/pages/Doctors.js
+++ client/src/pages/Doctors.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 const Doctors = ()=> {
+  const [doctors, setDoctors] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  
+
+
+  useEffect(()=>{
+    getDoctors()
+  },[])
+
+  const getDoctors = async()=>{
+    setError(null)
+    try{
+        let res = await axios.get('/api/doctors')
+                  setDoctors(res.data)
+                  setLoading(false)
+    }catch (error) {
+      setError(error)
+      setLoading(false)
+
+    }
+  }
+
+
+  if(loading) return <p>loading</p>
+  if(error) return <p>Error</p>
   return (
     <div>
       <h1>Doctors</h1>
+        <p>{JSON.stringify(doctors)}</p>
     </div>
   )
 }
